09025bbb78fd8f37f436e327d331a433a47e55d1
João Pedro Gomes <jgomes@dbg.local>
2020-03-30 13:32:04 -0300

    add react hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
 import { clickButton } from './actions'
 import './App.css';
 
-class App extends Component {
-  state = {
-    inputValue: ''
-  }
+export default function App(){
+  const newValue = useSelector(store => store.clickState.newValue)
+  const dispatch = useDispatch()
 
-  inputChange = e => {
-    this.setState({
-      inputValue: e.target.value
-    })
-  }
-
-  render(){
-    const {
-      clickButton,
-      newValue
-    } = this.props;
-
-    const { inputValue } = this.state;
-    
-    return (
-      <div className="App">
-        <input 
-          type="text"
-          onChange={this.inputChange}
-          value={inputValue}
-        />
-        <button onClick={() => clickButton(inputValue)}>
-          Click
-        </button>
-        <h1>{newValue}</h1>
-      </div>
-    );
-  }
+  const [inputValue, setInputValue] = useState('');
+  
+  return (
+    <div className="App">
+      <input 
+        type="text"
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+      />
+      <button onClick={() => dispatch(clickButton(inputValue))}>
+        Click
+      </button>
+      <h1>{newValue}</h1>
+    </div>
+  );
 }
-
-const mapStateToProps = store => ({
-  newValue: store.clickState.newValue
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ clickButton }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
